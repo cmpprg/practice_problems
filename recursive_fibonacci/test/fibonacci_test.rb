d13c3bacee32ae8f6ec4d3af52c1840b72c52821
Ryan Camp <campryan@comcast.net>
2020-08-27 14:20:08 -0600

    Add file for recursive_fibonacci

---
+++ recursive_fibonacci/test/fibonacci_test.rb
@@ -0,0 +1,20 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+
+class FibonacciTest < Minitest::Test
+  def setup
+    @fib = Fibonacci.new
+  end
+
+  def test_it_exists
+    assert_instance_of Fibonacci, @fib
+  end
+
+  def test_fibonacci_method_works
+    skip
+    assert_equal 3, @fib.position(4)
+    assert_equal 1, @fib.position(1)
+    assert_equal 13, @fib.position(7)
+    assert_equal 21, @fib.position(8)
+  end
+end
