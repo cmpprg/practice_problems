897c3074f4186f0ae2e130203b0a03b5f71bb595
Ryan Camp <campryan@comcast.net>
2020-08-30 21:25:41 -0600

    Set style for todo item component

--- vueJSExploration/src/components/TodoItem.vue
+++ vueJSExploration/src/components/TodoItem.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <p></p>
+    <p>Todo Item</p>
   </div>
 </template>
 
@@ -11,5 +11,23 @@
 </script>
 
 <style scoped>
+  .todo-item {
+    background: #f4f4f4;
+    padding: 10px;
+    border-bottom: 1px #ccc dotted;
+  }
 
+  .is-complete {
+    text-decoration: line-through;
+  }
+
+  .del {
+    background: #ff0000;
+    color: #fff;
+    border: none;
+    padding: 5px 9px;
+    border-radius: 50%;
+    cursor: pointer;
+    float: right;
+  }
 </style>
