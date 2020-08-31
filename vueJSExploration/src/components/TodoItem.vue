3da607b14fd3ae31029c5c389605748ec25d3618
Ryan Camp <campryan@comcast.net>
2020-08-31 08:24:23 -0600

    Complete checkbox and del function for todo, set up header

--- vueJSExploration/src/components/TodoItem.vue
+++ vueJSExploration/src/components/TodoItem.vue
@@ -1,12 +1,24 @@
 <template>
-  <div>
-    <p>Todo Item</p>
+  <div class="todo-item" v-bind:class="{'is-complete': todo.completed}">
+    <p>
+      <input type="checkbox" v-on:change="markComplete">
+      {{todo.title}}
+      <button v-on:click="$emit('del-todo', todo.id)" class="del">x</button>
+    </p>
   </div>
 </template>
 
 <script>
   export default {
-    name: 'TodoItem'
+    name: 'TodoItem',
+    props: {
+      todo: Object
+    },
+    methods: {
+      markComplete() {
+        this.todo.completed = !this.todo.completed
+      }
+    }
   }
 </script>
 
