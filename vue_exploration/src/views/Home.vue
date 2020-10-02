7580090759e39326993ec8e4133a7743f9ebab4a
Ryan Camp <campryan@comcast.net>
2020-10-02 15:54:16 -0600

    Add Todos component and set up vuex with axios call to jsonplaceholder

--- vue_exploration/src/views/Home.vue
+++ vue_exploration/src/views/Home.vue
@@ -1,18 +1,28 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div class="container">
+      <Todos />
+    </div>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import Todos from '@/components/Todos.vue'
 
 export default {
   name: 'Home',
   components: {
-    HelloWorld
+    Todos
   }
 }
 </script>
+
+<style>
+  .container {
+    max-width: 1100px;
+    margin: auto;
+    overflow: auto;
+    padding: 0 2rem;
+  }
+</style>
